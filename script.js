0fba10e14ad41cf5d9bcf5956c7b394047af7f3b
Putin9250 <sahil2404205@gmail.com>
2025-03-15 15:05:07 +0530

    Added Alert Via VS code

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
 var navLinks = document.getElementById("navLinks");
-    
-            function showMenu() {
-                navLinks.style.right = "0";
-            }
-    
-            function hideMenu() {
-                navLinks.style.right = "-200px";
-            }
 
-homeAlert=document.querySelector(".HomeAlert");
+function showMenu() {
+  navLinks.style.right = "0";
+}
 
-homeAlert.addEventListener("click", () => {
-    alert("Already At Home Page")
-})
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
 
-aboutAlert=document.querySelector("#HomeAlert");
+const homeAlert = document.querySelector(".HomeAlert");
+const aboutAlert = document.querySelector(".aboutAlert");
+const courseAlert = document.querySelector(".courseAlert");
+const blogAlert = document.querySelector(".blogAlert");
+const contactAlert = document.querySelector(".contactAlert");
+if (homeAlert) {
+  homeAlert.addEventListener("click", () => {
+    alert("Already At Home Page");
+  });
+}
 
-aboutAlert.addEventListener("click", () => {
-    alert("Already At About Page")
-})
+if (aboutAlert) {
+  aboutAlert.addEventListener("click", () => {
+    alert("Already At About Page");
+  });
+}
+
+if (courseAlert) {
+    courseAlert.addEventListener("click", () => {
+    alert("Already At Course Page");
+  });
+}
+
+if (blogAlert) {
+    blogAlert.addEventListener("click", () => {
+    alert("Already At Course Page");
+  });
+}
+
+if (contactAlert) {
+    contactAlert.addEventListener("click", () => {
+    alert("Already At Course Page");
+  });
+}
